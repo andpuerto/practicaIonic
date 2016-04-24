3215f1560bb4691240cae5041d6a7ddb01ac6f57
andpuerto <andpuerto@hotmail.com>
2016-04-24 13:00:12 +0200

    Cambiado el uso de $scope para compartir variables y metodos entre los controladores y las vistas por el uso self (en los controladores) y ng-controller (en las vistas). Esto ultimo se considera buena practica de programacion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,13 +28,15 @@ angular.module('starter.controllers', [])
 })
 
   //Controlador para las funciones de login
-.controller('LoginCtrl', ['$scope','$ionicPopup', '$state', 'Login', function($scope, $ionicPopup, $state, Login) {
-  $scope.logindata = {};
+.controller('LoginCtrl', ['$ionicPopup', '$state', 'Login', function($ionicPopup, $state, Login) {
+  var self=this;
+
+  self.logindata = {};
 
   //Funcion que se llamara cuando se realice el login
-  $scope.login = function() {
+  self.login = function() {
     //Llamamos al servicio de login para que se envien los datos al servidor
-    Login.enter($scope.logindata.username, $scope.logindata.password)
+    Login.enter(self.logindata.username, self.logindata.password)
       .success(function (data, status, headers, config) {
         //Si la atransmision ha sido correcta, comprobamos el estado
         if(data.status=='OK'){
@@ -60,23 +62,24 @@ angular.module('starter.controllers', [])
 
 
 //Controlador para la lista de libros
-.controller('LibrosCtrl', function($scope, $ionicPopup, Libros) {
+.controller('LibrosCtrl', ['$ionicPopup', 'Libros', '$scope', function($ionicPopup, Libros, $scope) {
+  var self = this;
   //Pagina cargada actualmente
-  $scope.paginaLibros=0;
+  self.paginaLibros=0;
   //Boolean para indicar si se han cargado todos los libros del servidor
-  $scope.todosCargados=false;
+  self.todosCargados=false;
   //Array con todos los libros de la lista
-  $scope.listaLibros=[];
+  self.listaLibros=[];
 
   //Funcion para cargar mas libros. Se le llamara cada vez que se quiera cargar una
   //Pagina nueva
-  $scope.cargarMasLibros = function(){
+  self.cargarMasLibros = function(){
     //Mensajes de error
     var errordatos='No se pudieron leer los datos';
     var tituloerror='Error';
 
     //Obtenemos los datos desde el servicio de libros. Le pasamos el siguiente numero de pagina
-    Libros.getLista(++$scope.paginaLibros)
+    Libros.getLista(++self.paginaLibros)
       .success(function (data, status, headers, config) {
         //Si la recepcion de informacion ha sido correcta, tenemos que comprobar el estado
         if(data.status!='KO'){
@@ -84,7 +87,7 @@ angular.module('starter.controllers', [])
           //Los agregamos a la lista y avisamos de que se ha terminado la accion para
           //el infinite scroll
           for (i in data){
-            $scope.listaLibros.push(data[i]);
+            self.listaLibros.push(data[i]);
           }
           $scope.$broadcast('scroll.infiniteScrollComplete');
         }else{
@@ -92,8 +95,8 @@ angular.module('starter.controllers', [])
           //pagina, por lo que establecemos a verdadero el valor de la variable
           //Aun asi, si recibimos un KO en la primera pagina, avisamos al usuario de que
           //no se han podido cargar los datos
-          $scope.todosCargados = true;
-            if($scope.paginaLibros==1){
+          self.todosCargados = true;
+            if(self.paginaLibros==1){
             $ionicPopup.alert({
               title: tituloerror,
               template: errordatos
@@ -111,4 +114,4 @@ angular.module('starter.controllers', [])
 
   }
 
-});
+}]);
